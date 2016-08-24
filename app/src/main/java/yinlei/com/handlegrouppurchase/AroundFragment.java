b42de36af6e162856e8117a7b31bad49f61950b4
wuyinlei <wuyinlei19931118@hotmail.com>
2016-08-24 21:21:57 +0800

    使用fragmenttabhost实现底部菜单栏

---
+++ app/src/main/java/yinlei/com/handlegrouppurchase/AroundFragment.java
@@ -0,0 +1,26 @@
+package yinlei.com.handlegrouppurchase;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 在此写用途
+ *
+ * @version V1.0 <描述当前版本功能>
+ * @FileName: yinlei.com.handlegrouppurchase.AroundFragment.java
+ * @author: myName
+ * @date: 2016-08-24 21:04
+ */
+
+public class AroundFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
--- app/src/main/java/yinlei/com/handlegrouppurchase/MainActivity.java
+++ app/src/main/java/yinlei/com/handlegrouppurchase/MainActivity.java
@@ -1,13 +1,50 @@
 package yinlei.com.handlegrouppurchase;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //fragment
+    private Class[] fragments = new Class[]{
+            MainFragment.class,AroundFragment.class,MineFragment.class
+            ,MoreFragment.class
+    };
+
+    //图片资源
+    private int [] imgResources = new int[]{
+            R.drawable.ic_home_tab_index_selector,
+            R.drawable.ic_home_tab_near_selector,
+            R.drawable.ic_home_tab_my_selector,
+            R.drawable.ic_home_tab_more_selector
+    };
+
+    //标题
+    private String[] tabIitles = new String[]{"首页","周边","我的","更多"
+    ,};
+
+    FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost) findViewById(R.id.tabHost);
+        mTabHost.setup(this,getSupportFragmentManager(),android.R.id.tabcontent);
+
+        for (int i = 0; i < fragments.length; i++) {
+            View view = getLayoutInflater().inflate(R.layout.tab_item,null);
+            TextView tab_tv = (TextView) view.findViewById(R.id.tv);
+            ImageView tab_iv = (ImageView) view.findViewById(R.id.iv);
+            tab_iv.setImageResource(imgResources[i]);
+            tab_tv.setText(tabIitles[i]);
+            mTabHost.addTab(mTabHost.newTabSpec(""+i).setIndicator(view), fragments[i], null);
+
+        }
+
     }
 }
