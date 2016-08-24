5efb49e2eb926f43a3c3a2ed112dc1bfa2af61b7
wuyinlei <wuyinlei19931118@hotmail.com>
2016-08-24 20:57:35 +0800

    仿拉手团购，项目搭建

---
+++ app/src/main/java/yinlei/com/handlegrouppurchase/MainActivity.java
@@ -0,0 +1,13 @@
+package yinlei.com.handlegrouppurchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
